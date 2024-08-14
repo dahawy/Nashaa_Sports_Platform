e7656e0f16e829ef456343e70af563523e4b7790
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-08-14 22:06:58 +0300

    Initiate project

---
+++ NashaaSports/NashaaSports/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for NashaaSports project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path ,include
+from django.conf.urls.static import static
+from . import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("main.urls")),
+    path("account/", include("account.urls")),
+    path("academy/", include("academy.urls")),
+    path("bookmark/", include("bookmark.urls")),
+    path("cart/", include("cart.urls")),
+    path("client/", include("client.urls")),
+    path("contactUs/", include("contactUs.urls")),
+    path("enrollment/", include("enrollment.urls")),
+    path("review/", include("review.urls")),
+    
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # -Nashaa_Sports_Platform
+## Description:
+
+This is a platform for sports academies and clubs to advertise for their sports programs for kids and youngsters. It allows Academies to add their programs, manage the enrollments, check the incomes, and more.
+
+The platform also allows parents or youngsters to sign up and check the existing programs based on different categories like location, sports, and price and choose what to register to.
+
+## Development Team:
+
+1- Abdullah Aldhamen
+2- Saeed Alghamdi
+3- Abdulaziz Asiri (Team Leader)
+4- Dhahoi Alharbi
