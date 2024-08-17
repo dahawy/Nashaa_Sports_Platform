1b6414af07d9af03ee060ca22f319ce5b0c2960a
D. F <ifonei@hotmail.com>
2024-08-17 22:44:32 +0300

    creating contact us from client side

--- NashaaSports/NashaaSports/settings.py
+++ NashaaSports/NashaaSports/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'client',
     'contactUs',
     'enrollment',
+    'admin',
 ]
 
 MIDDLEWARE = [
--- NashaaSports/contactUs/admin.py
+++ NashaaSports/contactUs/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from contactUs.models import CustomerQuery
 
 # Register your models here.
+
+admin.site.register(CustomerQuery)
