6021e3af8e4e3ea58ba7f2215c26cdc2429dc64b
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-08-17 15:32:59 +0300

    create user and academy profile

--- NashaaSports/account/urls.py
+++ NashaaSports/account/urls.py
@@ -5,6 +5,9 @@ app_name = "account"
 
 urlpatterns = [
     path('signup/', views.sign_up, name="sign_up"),
+    path('signup/as/academy', views.sing_up_asAcademy_view, name="sing_up_asAcademy_view"),
     path('login/', views.log_in, name="log_in"),
     path('logout/', views.log_out, name="log_out"),
+    path('profile/<user_name>/',views.profile_view, name="profile_view"),
+    path('addProfile/<user_name>/',views.create_profile_view, name="create_profile_view")
 ]
