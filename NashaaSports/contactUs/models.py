3e70335d86550e1d9ecb312518eb01c6e9801a22
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-08-18 14:33:22 +0300

    change customer us models

--- NashaaSports/contactUs/models.py
+++ NashaaSports/contactUs/models.py
@@ -4,11 +4,18 @@ class CustomerQuery(models.Model):
         Complaint = 'Complaint', 'شكوى'
         Query = 'Query' , 'استفسار'
         Suggestion= 'Suggestion' , 'اقتراح'
+    class StatusOfMessage(models.TextChoices):
+        Open = 'Open', 'مفتوح'
+        Closed = 'Closed' , 'مغلق'
+    class SenderType(models.TextChoices):
+        Academy = 'Academy', 'أكاديمية'
+        Individual = 'Individual' , 'فرد'
 
     name = models.CharField(max_length=128)
+    status = models.CharField(choices=StatusOfMessage.choices, default=StatusOfMessage.Open)
+    sender_type = models.CharField(choices=SenderType.choices, default=SenderType.Individual)
     email = models.EmailField()
     subject = models.CharField(max_length=128)
     message = models.TextField()
     category = models.CharField(choices=QueryCategory.choices, default= QueryCategory.Query) 
     created_at = models.DateTimeField(auto_now_add=True)
-
