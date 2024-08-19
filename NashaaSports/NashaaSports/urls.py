be74d93a32cc74440767d1fd46bf31e65e7cba16
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-08-19 11:07:24 +0300

    complete reset_password functionality

--- NashaaSports/NashaaSports/urls.py
+++ NashaaSports/NashaaSports/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path ,include
 from django.conf.urls.static import static
 from . import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,7 +32,8 @@ urlpatterns = [
     path("enrollment/", include("enrollment.urls")),
     path("review/", include("review.urls")),
     path("moderator/", include("moderator.urls")),
-    
-   
-    
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='reset_password.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='reset_password_done.html'), name='password_reset_done'),
+    path('reset/confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name="password_reset_confirm"),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
