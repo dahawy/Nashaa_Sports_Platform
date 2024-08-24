dbac317e766704e700f50f1a380222de6a675b84
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-08-24 15:26:01 +0300

    fix user registration &  design cart and payment layout

--- NashaaSports/cart/views.py
+++ NashaaSports/cart/views.py
@@ -7,11 +7,15 @@ def cart_summary_view (request:HttpRequest, cart_id):
     programs = Program.objects.all()
     carts = Cart.objects.get(id=cart_id)
     enrollments = carts.enrollments.all() if carts else []
+    total_fees = sum(enrollment.program.fees for enrollment in enrollments)
+    total_item= enrollments.count()
     print(carts)
     context ={
         "programs":programs,
         "carts":carts,
         "enrollments":enrollments,
+        "total_item":total_item,
+        "total_fees":total_fees,
     }
 
 
